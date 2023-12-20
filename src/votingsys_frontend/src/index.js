7da5e6fc6b00af0651fe36250c9ef93edea5d47f
Dennis Kiarie <dennizwarui@gmail.com>
2023-12-20 13:28:48 +0300

    init commit

---
+++ src/votingsys_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { votingsys_backend } from "../../declarations/votingsys_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await votingsys_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
